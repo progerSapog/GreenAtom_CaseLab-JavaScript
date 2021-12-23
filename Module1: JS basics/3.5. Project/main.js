ac8139f4a504966cc070961555a585b720a9da85
Vladislav <vlad.sapozhnickof@gmail.com>
2021-12-23 14:20:48 +0300

    Project

---
+++ Module1: JS basics/3.5. Project/main.js
@@ -0,0 +1,42 @@
+function censor()
+{
+    //Массив для хранения пар, переданных как 2 аргумента
+    const pairArr = [];
+
+    /**
+     * Работает по разному в зависимости от кол-ва параметров:
+     *   1 параметр  - замена слов в переданной строке соглсано парам
+     *   2 параметра - запись данной пары слов в массив пар
+    */
+    function pushPair(el1, el2 = undefined)
+    {
+        //1 параметр - замена слов
+        if (el2 === undefined)
+        {
+            for (let pair of pairArr)
+            {
+                if (el1.includes(pair[0]))
+                {
+                    el1 = el1.replace(pair[0], pair[1]);
+                }
+            }
+            return el1;
+        }
+        //ДВа параметра - записываем пару
+        else
+        {
+            pairArr.push([el1, el2])
+        }
+    }
+
+    return pushPair;
+}
+
+
+const changeScene = censor();
+
+changeScene('PHP','JS');
+
+changeScene('backend', 'frontend')
+
+console.log(changeScene('PHP is the most popular programming language for backend web-development'));
