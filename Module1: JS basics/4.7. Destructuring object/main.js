a376c5f55eeff6419283c6f799c2e43f13d6752e
Vladislav <vlad.sapozhnickof@gmail.com>
2021-12-25 17:11:55 +0300

    Dist obj

---
+++ Module1: JS basics/4.7. Destructuring object/main.js
@@ -0,0 +1,26 @@
+const link = {
+    href: '#',
+    title: 'simple link',
+    target: 'blank',
+    className: 'link',
+    id: null,
+    children: {
+        span: {
+            content: 'Click me',
+            className: 'anchor',
+        },
+    },
+}
+
+const {
+    target,
+    className,
+    children: {
+        span: {
+            content,
+            className: renamed
+        }
+    }
+} = link || {};
+
+console.log(target, className, renamed, content);
