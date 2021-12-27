01439df0c99a6984d57f07fea53ea075cf3d56bd
Vladislav <vlad.sapozhnickof@gmail.com>
2021-12-27 18:12:46 +0300

    get HTML elements

---
+++ Module1: JS basics/5.1. Get HTML elements/app.js
@@ -0,0 +1,4 @@
+const title = document.getElementsByTagName('h1')
+const btn = document.getElementById('btn')
+const subtitle = document.querySelector('.title-gray')
+const sections = document.getElementsByTagName('section')
